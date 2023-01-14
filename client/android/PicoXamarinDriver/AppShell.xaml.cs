f708efa40796c829d3a18f6d16fe97d29603b3e4
Grzegorz <90092477+grzesiekk013@users.noreply.github.com>
2023-01-14 21:25:11 +0100

    Add files via upload

---
+++ client/android/PicoXamarinDriver/AppShell.xaml.cs
@@ -0,0 +1,17 @@
+﻿using PicoXamarinDriver.ViewModels;
+using PicoXamarinDriver.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace PicoXamarinDriver
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ client/android/PicoXamarinDriver/Views/SettingsPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using PicoXamarinDriver.Models;
+using PicoXamarinDriver.ViewModels;
+using PicoXamarinDriver.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PicoXamarinDriver.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        SettingsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new SettingsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+           // _viewModel.OnAppearing();
+        }
+
+        private void Switch_Toggled(object sender, ToggledEventArgs e)
+        {
+
+        }
+    }
+}
